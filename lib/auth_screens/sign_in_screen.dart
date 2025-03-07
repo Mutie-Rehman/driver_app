4e0cb9cfdc1dc4f44c7172108bd523220cb82338
Mutie-Rehman <133040078+Mutie-Rehman@users.noreply.github.com>
2025-03-07 19:37:19 +0500

    Sign in screen designed

---
+++ lib/auth_screens/sign_in_screen.dart
@@ -0,0 +1,100 @@
+import 'package:driver_app/auth_screens/sign_up_screen.dart';
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Image.asset(
+                  "images/logo_driver_app.jpg",
+                  height: 200,
+                  width: 200,
+                ),
+                const Text(
+                  "Driver Sign In",
+                  style: TextStyle(fontSize: 32),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                //textfields
+                //email
+                TextField(
+                  controller: emailController,
+                  style: const TextStyle(color: Colors.grey),
+                  decoration: const InputDecoration(
+                    labelText: "Email",
+                    hintText: "Email",
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    hintStyle: TextStyle(color: Colors.grey),
+                    labelStyle: TextStyle(color: Colors.grey),
+                  ),
+                ),
+                //password
+                TextField(
+                  controller: passwordController,
+                  style: const TextStyle(color: Colors.grey),
+                  decoration: const InputDecoration(
+                    labelText: "Password",
+                    hintText: "Password",
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    hintStyle: TextStyle(color: Colors.grey),
+                    labelStyle: TextStyle(color: Colors.grey),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text("Dont have an Account"),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const SignUpScreen()));
+                        },
+                        child: const Text("Sign Up"))
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
