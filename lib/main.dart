f61f39754c58736ba77e7965b6a3deb1163c9576
Mutie-Rehman <133040078+Mutie-Rehman@users.noreply.github.com>
2025-03-08 19:43:32 +0500

    firebase connection done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:driver_app/firebase_options.dart';
 import 'package:driver_app/splash_screen/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized;
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp(
       child: const MaterialApp(
           debugShowCheckedModeBanner: false,
