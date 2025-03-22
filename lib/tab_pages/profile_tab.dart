41c27238b4a5ed17ce561ca8dd53dc3a32bc69fd
Mutie-Rehman <133040078+Mutie-Rehman@users.noreply.github.com>
2025-03-22 13:02:49 +0500

    logout button added

--- lib/tab_pages/profile_tab.dart
+++ lib/tab_pages/profile_tab.dart
@@ -1,3 +1,6 @@
+import 'package:driver_app/auth_screens/sign_in_screen.dart';
+import 'package:driver_app/global/global.dart';
+import 'package:driver_app/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 class ProfileTab extends StatefulWidget {
@@ -10,6 +13,15 @@ class ProfileTab extends StatefulWidget {
 class _ProfileTabState extends State<ProfileTab> {
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Profile Tab"));
+    return Center(
+        child: ElevatedButton(
+            onPressed: () {
+              fAuth.signOut();
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const MySplashScreen()));
+            },
+            child: const Text("Logout")));
   }
 }
